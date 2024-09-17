80e5363669a824424ab417c6bca521c529e8779f
p++ <mel-garr@student.1337.ma>
2024-09-17 16:56:57 +0100

    m

---
+++ project/game/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('home/', home, name='home'),
+    path('auth-status/', check_login_status, name='auth_status'),
+    path('login/', login, name='loginO'),
+    path('lobby/', lobby, name='lobby'),
+    path('offmulti/', offmulti, name='offmulti'),
+
+
+    # path('login3/', login3, name='login3'),
+]
---
+++ project/game/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse, HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+
+
+def home(request):
+    context = {}
+    try :
+        user_id = request.session.get('user_id')
+        if user_id:
+            user = UserL.objects.get(id=user_id)
+            context = {'user': user}
+        else:
+            context = {}
+    except:
+            context ={}
+    
+    return render (request, 'home.html', context)
+
+def lobby(request):
+    context = {}
+    try :
+        user_id = request.session.get('user_id')
+        if user_id:
+            user = UserL.objects.get(id=user_id)
+            context = {'user': user}
+        else:
+            context = {}
+    except:
+            context ={}
+    return render(request, 'game/lobby.html', context)
+
+# @csrf_exempt
+def check_login_status(request):
+    user_id = request.session.get('user_id')
+    if user_id:
+        try:
+            user = UserL.objects.get(id=user_id)
+            status = user.status
+            return JsonResponse({'is_logged_in' : True , 'status': user.status})
+        except UserL.DoesNotExist:
+            return JsonResponse({'is_logged_in' : False})
+    else:
+        return JsonResponse({'is_logged_in' : False})
+    
+# @csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            loginId = data.get('LoginId')
+            logPass = data.get('password')
+
+            if not loginId or not logPass:
+                return JsonResponse({'success': False, 'error': 'give us both login and password'}, status=400)
+
+            user = UserL.objects.get(name=loginId, password=logPass)
+            request.session['user_id'] = user.id
+            return JsonResponse({'success': True})
+        except UserL.DoesNotExist:
+            return JsonResponse({'success': False, 'error': 'user/pass does not exist in the db'}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'error': 'Invalid json data'}, status=400)
+        except Exception as x :
+            return JsonResponse({'success': False, 'error': f'{x}'})
+    
+    return render(request, '/home.html')
+
+    
+
+# def pick_mode(request):
+#     if request.method == 'POST':
+#         game_type = request.POST.get('game_type')
+#         mode = request.POST.get('mode')
+
+#         request.session['game_type'] = game_type
+#         request.session['mode'] = mode
+
+#         return 
+    
+def offmulti(request):
+    return render(request, 'game/offmulti.html')
